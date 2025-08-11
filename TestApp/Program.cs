64d359f705693c07c01d66cb249ee84f522d34ad
Kyrylo <popovkirill070320@gmail.com>
2025-08-11 16:21:34 +0300

    Revert "Merge pull request #4 from Kirillius7/feature/new-function"
    This reverts commit d0833e4537744f0138b5f24887a29485e940af71, reversing
    changes made to 8332672609a1914753eec69484c45c38d8e49da1.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -173,12 +173,7 @@ internal class Program
         $"result: {a.GetType()}";
     //delegate, function, event, funcs in linq
 
-    static int Result(int a, int b) => a * b;
 
-    static void OutputResult(string msg, int res)
-    {
-        Console.WriteLine($"{msg}: {res}");
-    }
     static void Main(string[] args)
     {
         using (var context = new AppDbContext())
@@ -286,7 +281,7 @@ internal class Program
         }
 
         Console.WriteLine("Happy birthday!");
-        OutputResult("the outcome is ", Result(33, 123));
+
         Console.ReadLine();
     }
 }
